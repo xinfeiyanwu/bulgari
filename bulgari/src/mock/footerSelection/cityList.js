732f7378e4fe182fb2552b81fa280d384dd285da
李欣欣 <820196673@qq.com>
2020-04-08 15:49:07 +0800

    删除文件 bulgari

--- bulgari/src/mock/footerSelection/cityList.js
+++
@@ -1,9 +0,0 @@
-import Mock from 'mockjs'
-
-const cityList = [
-    {name: '北京', value:1},
-    {name: '上海', value:2},
-    {name: '海口', value:3},
-]
-
-export default Mock.mock('/cityList', 'get', cityList)
--- bulgari/src/mock/userOperation/register.js
+++
@@ -1,5 +0,0 @@
-import Mock from 'mockjs'
-
-const VerificatCode = Math.ceil(Math.random()*10000)
-
-export default Mock.mock('/resister/getVerificatCode', 'get', VerificatCode)
